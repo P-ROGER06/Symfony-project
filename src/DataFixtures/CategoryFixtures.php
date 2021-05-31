3b908938c967037cb399169c7bf025e44111d58c
P-ROGER06 <proger06@gmail.com>
2021-05-31 22:03:15 +0200

    quest9

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Action',
+        'Aventure',
+        'Animation',
+        'Fantastique',
+        'Horreur',
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $key => $name) {
+            $category = new Category();
+            $category->setName($name);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
